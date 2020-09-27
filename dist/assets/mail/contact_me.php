2c00fcabd3dcf2e2e93877ed4814d1e9fc5939fc
Corina Barbalata <cbarbalata@users.noreply.github.com>
2020-09-27 11:34:15 -0500

    Update contact_me.php

--- dist/assets/mail/contact_me.php
+++ dist/assets/mail/contact_me.php
@@ -16,7 +16,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = "cbarbalata@lsu.edu"; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'cbarbalata@lsu.edu'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: cbarbalata@lsu.edu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
